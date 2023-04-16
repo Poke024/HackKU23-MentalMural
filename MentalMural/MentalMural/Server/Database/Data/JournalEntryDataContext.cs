26a73103479994eff9029a27985a125c713a6230
torresHK <adair.tor24@gmail.com>
2023-04-16 01:14:13 -0500

    Pushing some Journal UI Updates

---
+++ MentalMural/MentalMural/Server/Database/Data/JournalEntryDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MentalMural.Shared;
+
+namespace MentalMural.Server.Database.Data
+{
+    public class JournalEntryDataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public JournalEntryDataContext(IConfiguration configuration, DbContextOptions<JournalEntryDataContext> options) : base(options)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("JournalEntryDB"));
+        }
+
+
+        // when adding any new set, perform the following commands to update the database and add a table
+        // dotnet ef migrations add [insert-migration-name]
+        // dotnet ef database update
+        public DbSet<JournalEntryData> JournalEntries { get; set; }
+        public DbSet<UserData> UserInfo { get; set; }
+    }
+
+}
